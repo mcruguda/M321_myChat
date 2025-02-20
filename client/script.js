2266ff3201d2ea1866942cd55f81add39e2932d5
Cyrill Augustiny <c.augustiny@gmail.com>
2025-02-20 08:55:12 +0100

    Messages
    Changed the layot for messages sent and added time / date to it

--- client/script.js
+++ client/script.js
@@ -16,9 +16,36 @@ socket.addEventListener("open", (event) => {
 });
 
 const createMessage = (message) => {
-  const p = document.createElement("p");
-  p.textContent = message;
-  document.getElementById("messages").appendChild(p);
+  const jsonMessage = JSON.parse(message);
+  if (jsonMessage.type !== "message") return;
+  document.getElementById(
+    "chatBox"
+  ).innerHTML += `<div class="w-full flex justify-end mt-3">
+                <div class="w-1/2">
+                  <div class="flex items-center justify-end">
+                    <p class="font-semibold mr-3 text-sm text-slate-600">
+                      Me <span class="text-slate-400 text-xs">${jsonMessage.date} - ${jsonMessage.time}</span>
+                    </p>
+
+                    <img
+                      class="h-5 w-5 overflow-hidden rounded-full"
+                      src="https://source.unsplash.com/random/500x500/?face"
+                      alt=""
+                    />
+                  </div>
+
+                  <div
+                    class="mt-3 w-full bg-blue-500 p-4 rounded-b-xl rounded-tl-xl"
+                  >
+                    <p class="text-sm text-white">
+                      ${jsonMessage.text}
+                    </p>
+                  </div>
+                </div>
+              </div>`;
+  // const messageBox = document.createElement("");
+  // messageBox.textContent = message;
+  // document.getElementById("messages").appendChild(p);
 };
 
 // Listen for messages from server
@@ -40,9 +67,17 @@ socket.addEventListener("error", (event) => {
 document.addEventListener("DOMContentLoaded", () => {
   document.getElementById("btnSendHello").addEventListener("click", () => {
     const messageInput = document.getElementById("inptMessage").value;
+    const currentDate = new Date();
+    const day = currentDate.getDate();
+    const month = currentDate.getMonth() + 1;
+    const year = currentDate.getFullYear();
+    const hour = currentDate.getHours();
+    const minute = currentDate.getMinutes();
     const message = {
       type: "message",
       text: messageInput,
+      date: `${day}/${month}/${year}`,
+      time: `${hour}:${minute}`,
     };
     socket.send(JSON.stringify(message));
   });
